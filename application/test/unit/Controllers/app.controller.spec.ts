e5145be8ae771f0d78173971a6bf4728080a88e8
rcastro <cruzcastro07@gmail.com>
2020-06-26 21:30:17 +0100

    test: cover app controller with unit tests

--- application/test/unit/Controllers/app.controller.spec.ts
+++ application/test/unit/Controllers/app.controller.spec.ts
@@ -1,22 +1,67 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from '../../../src/Controllers/app.controller';
-import { AppService } from '../../../src/Services/app.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {AppController} from '../../../src/Controllers/app.controller';
+import {AppService} from '../../../src/Services/app.service';
+import {AppServiceInterface} from "../../../src/Services/Interfaces/app.service.interface";
+const data  = require('../../helpers/payloads/repository.json');
+
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppServiceInterface;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: 'AppServiceInterface',
+          useExisting: AppService,
+        },
+        {
+          provide: 'GitHubClientInterface',
+          useValue: {},
+        },
+        {
+          provide: 'RepositoryConverterInterface',
+          useValue: {},
+        },
+        {
+          provide: 'CACHE_MANAGER',
+          useValue: {},
+        }
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('getRepository', () => {
+    it('should return a RepositoryDto array', async () => {
+      const repositoryDtoData = data.repositoriesDto
+
+      const getRepositorySpy = jest
+          .spyOn(appService, 'getRepository')
+          .mockImplementation(async () => repositoryDtoData);
+
+      const expectedResult = JSON.parse(JSON.stringify(repositoryDtoData));
+
+      expect(await appController.getRepository('phakeUsername')).toEqual(expectedResult);
+      expect(getRepositorySpy).toHaveBeenCalledTimes(1);
     });
+
+    it('should return an empty RepositoryDto array', async() => {
+      const getRepositorySpy = jest
+          .spyOn(appService, 'getRepository')
+          .mockImplementation(async () => []);
+
+      expect(await appController.getRepository('phakeUsername')).toEqual([]);
+      expect(getRepositorySpy).toHaveBeenCalledTimes(1);
+    })
   });
 });
